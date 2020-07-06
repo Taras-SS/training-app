f1c0ebc6ef8f6e8890a373a3cf8cd9251e0bc52e
Nazar Loiko <43824356+Nazik111@users.noreply.github.com>
2020-07-06 16:03:53 +0300

    admin page

---
+++ src/pages/Admin/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, UseEffect, useState} from 'react';
+import { Header, Image, Menu, Ref, Segment, Sidebar } from 'semantic-ui-react'
+import s from './Sidebar.module.css'
+
+const MainSidebar = () => {
+
+
+    return (
+               <div className={s.sidebarlook}>
+                   <a href='#' className={s.sidebaritems}>Головна</a>
+                   <a href='#' className={s.sidebaritems}>Список товарів</a>
+                   <a href='#' className={s.sidebaritems}>Редагувати</a>
+                   <a href='#' className={s.sidebaritems}>Вихід</a>
+               </div>
+
+    )
+}
+
+export default MainSidebar;
