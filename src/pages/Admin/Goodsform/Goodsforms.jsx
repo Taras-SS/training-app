3c000cdb0c23c4c81ee18bb0fe7824814ce7ddf6
Taras Dyda <tarasdida22@gmail.com>
2020-07-06 18:14:53 +0300

    admmin page - merged

--- src/pages/Admin/Goodsform/Goodsforms.jsx
+++ src/pages/Admin/Goodsform/Goodsforms.jsx
@@ -2,14 +2,16 @@ import React, {useState, useEffect} from "react";
 import s from './Goodsform.module.css';
 import {Form, Button, TextArea, Image} from "semantic-ui-react";
 import {uploadImage} from "../../../api/goods";
-import {Uploader} from '@navjobs/upload';
-import axios from 'axios';
 import noimage from '../../../assets/images/noimage.png'
 
 const Goodsforms = () => {
 
     const [image, setImage] = useState(noimage);
-const [uploadedFile, setUploadedFile] = useState(null);
+    const [uploadedFile, setUploadedFile] = useState(null);
+
+    useEffect(() => {
+
+    }, [])
 
     const Onsubmit = async () => {
         const data = new FormData();
@@ -21,41 +23,33 @@ const [uploadedFile, setUploadedFile] = useState(null);
             crossDomain: true,
         }
         data.append("avatarImage", uploadedFile, uploadedFile.name);
-        const result = await uploadImage(data,requestOptions);
+        const result = await uploadImage(data, requestOptions);
         console.log(result.data.imageUrl);
 
     }
 
 
-
     const isFileValid = (file) => {
         if (!file) return false;
-
         const validExtentions = ["image/jpg", "image/png", "image/jpeg"];
         if (!validExtentions.includes(file.type)) {
-
             return false;
         }
         const maxSize = 15728640;
         if (maxSize < file.size) {
-
             return false;
         }
-
         return true;
     }
 
     const onChange = (e) => {
-
         const reader = new FileReader();
         const file = e.target.files[0];
         const valid = isFileValid(file);
         if (!valid) return;
-
         reader.onloadend = () => {
             setUploadedFile(file);
             setImage(reader.result);
-
         };
         reader.readAsDataURL(file);
     }
@@ -78,16 +72,16 @@ const [uploadedFile, setUploadedFile] = useState(null);
                         label='Опис товару'
                         placeholder='Опис повинен не перевищувати довжину у 40 символів.'
                     />
-                    <Button type='submit' onClick={() => Onsubmit() }>Зберегти</Button>
+                    <Button type='submit' onClick={() => Onsubmit()}>Зберегти</Button>
                 </Form>
-
             </div>
 
             <div className={s.imgWrapper}>
                 <Image size='medium' src={image}/>
                 <label>
-                    <div className={s.inputWrapper}><input className={s.fileLoader} type="file"
-                                                           onChange={(e) => onChange(e)}/> Загрузити файл
+                    <div className={s.inputWrapper}>
+                        <input className={s.fileLoader} type="file"
+                               onChange={(e) => onChange(e)}/> Загрузити файл
                     </div>
                 </label>
             </div>
--- src/pages/Admin/Sidebar/Sidebar.jsx
+++ src/pages/Admin/Sidebar/Sidebar.jsx
@@ -1,17 +1,17 @@
-import React, {useEffect, UseEffect, useState} from 'react';
-import { Header, Image, Menu, Ref, Segment, Sidebar } from 'semantic-ui-react'
+import React from 'react';
+import {Header, Image, Menu, Ref, Segment, Sidebar} from 'semantic-ui-react'
 import s from './Sidebar.module.css'
 
 const MainSidebar = () => {
 
 
     return (
-               <div className={s.sidebarlook}>
-                   <a href='#' className={s.sidebaritems}>Головна</a>
-                   <a href='#' className={s.sidebaritems}>Список товарів</a>
-                   <a href='#' className={s.sidebaritems}>Редагувати</a>
-                   <a href='#' className={s.sidebaritems}>Вихід</a>
-               </div>
+        <div className={s.sidebarlook}>
+            <a href='#' className={s.sidebaritems}>Головна</a>
+            <a href='#' className={s.sidebaritems}>Список товарів</a>
+            <a href='#' className={s.sidebaritems}>Редагувати</a>
+            <a href='#' className={s.sidebaritems}>Вихід</a>
+        </div>
 
     )
 }
